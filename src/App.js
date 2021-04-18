628f1a30763d0e2cc90eadd7c42d83f617ef1a7d
David Wolverton <dwolvert@gmail.com>
2021-04-18 10:08:07 -0400

    Stuff from David

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import Switch from './components/Switch';
 import Weather from './components/Weather';
 
 function App() {
+  const weatherData = [
+    { time: "Today", highF: 60, lowF: 50, conditions: "Mostly Cloudy" },
+    { time: "Tomorrow", highF: 65, lowF: 53, conditions: "Partly Sunny" },
+    { time: "Tuesday", highF: 66, lowF: 51, conditions: "Partly Sunny" }
+  ];
+
   return (
     <div className="App">
       <h1>This is React</h1>
-      <Weather time="Today" conditions="Overcast" tempF={45}/>
+      {
+        weatherData.map(data => (
+          <Weather key={data.time} time={data.time} conditions={data.conditions} tempF={data.highF}/>
+        ))
+      }
+
+      {/* <Weather time="Today" conditions="Overcast" tempF={45}/>
       <Weather time="Tomorrow" conditions="Sunny" tempF={58}/>
-      <Weather time="Tuesday" conditions="Thunderstorms" tempF={60}/>
+      <Weather time="Tuesday" conditions="Thunderstorms" tempF={60}/> */}
       <Switch />
       <Counter />
       <Counter />
