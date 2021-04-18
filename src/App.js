6db60c80c54ce71ab6ee75a9fd95730ee2fb8a38
David Wolverton <dwolvert@gmail.com>
2021-04-18 09:25:43 -0400

    Stuff from David

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Counter from './components/Counter';
 import Switch from './components/Switch';
+import Weather from './components/Weather';
 
 function App() {
   return (
     <div className="App">
       <h1>This is React</h1>
+      <Weather time="Today" conditions="Overcast" tempF={45}/>
       <Switch />
       <Counter />
     </div>
