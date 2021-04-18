6bd948eca80a9096ed83610f97f4367f5dcf6400
David Wolverton <dwolvert@gmail.com>
2021-04-18 08:51:44 -0400

    callback props demos

---
+++ src/components/keypad/CodeEntry.js
@@ -0,0 +1,19 @@
+import Keypad from "./Keypad";
+
+function CodeEntry() {
+  const [code, setCode] = useState("");
+
+  function appendCode(number) {
+    setCode(code + number);
+  }
+
+  return (
+    <div className="CodeEntry">
+      <h2>Enter Code</h2>
+      <p>{code || "...."}</p>
+      <Keypad onKey={appendCode} />
+    </div>
+  );
+}
+
+export default CodeEntry;
